8a4f03cc6764851da020004ff5abfb565723e01b
com-chaitu <ramachaitanya.g@gmail.com>
2019-07-18 22:51:11 -0500

    initial commit for donar list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { 
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule, 
+  MatSortModule, 
+  MatProgressSpinnerModule, 
+  MatButtonModule,
+  MatIconModule
+} from '@angular/material';
+import { DonarListComponent } from './donar-list/donar-list.component';
+
+
+const materialImports = [
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatProgressSpinnerModule,
+  MatButtonModule,
+  MatIconModule
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    DonarListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    ...materialImports
   ],
   providers: [],
   bootstrap: [AppComponent]
